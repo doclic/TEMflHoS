7e5fa34898f1a8b9f9bfb8f2a3639c9a24bef736
BlendyDev <github@blendy.dev>
2025-01-14 12:48:34 +0100

    idk it aint working

--- src/main/kotlin/me/doclic/temflhos/event/RenderItemOverlayPostEvent.kt
+++ src/main/kotlin/me/doclic/temflhos/event/RenderItemOverlayPostEvent.kt
@@ -3,4 +3,4 @@ package me.doclic.temflhos.event
 import net.minecraft.client.gui.FontRenderer
 import net.minecraft.item.ItemStack
 
-class RenderItemOverlayPostEvent (var fr: FontRenderer, var stack: ItemStack, var xPosition: Int, var yPosition: Int, var text: String)
+class RenderItemOverlayPostEvent (var fr: FontRenderer, var stack: ItemStack, var xPosition: Int, var yPosition: Int, var text: String): Event()
